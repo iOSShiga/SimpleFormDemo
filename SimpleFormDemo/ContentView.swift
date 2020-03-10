b1a1f40ef9271571e5985c4cdc9e7d3c75ec9ea5
Shigasuresh@123 <shigasuresh546@gmail.com>
2020-03-10 17:25:54 +0530

    initial

--- SimpleFormDemo/ContentView.swift
+++ SimpleFormDemo/ContentView.swift
@@ -9,8 +9,107 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var pinON: Bool = true
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            
+            Form {
+                
+                Section(header: Text("Payment Methods").bold()) {
+                    
+                    HStack {
+                        
+                        Text("Mastercard***6390")
+                        
+                        Spacer()
+                        
+                        Image("mast")
+
+                    }
+                    
+                    Text("Expires 11/24")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                    
+                    HStack {
+                        
+                        Image(systemName: "folder")
+                            .font(Font.system(size: 25.0))
+                            .foregroundColor(.gray)
+                        
+                        Text("Add new card")
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            
+                            Image(systemName: "plus.circle.fill")
+                                    .font(Font.system(size: 30))
+                        }
+                    }
+                    
+                    
+                    HStack {
+                        
+                        Image(systemName: "folder")
+                            .font(Font.system(size: 25.0))
+                            .foregroundColor(.gray)
+                        
+                        Text("Add paypal")
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .font(Font.system(size: 30))
+                        }
+                    }
+                }
+                
+                
+                Section(header: Text("Payment History").bold()) {
+                    Text("No completed transactions")
+                }
+                
+                Section(header: Text("Security").bold()) {
+                    Toggle(isOn: $pinON) {
+                        Text("Pin ON")
+                    }
+                }
+                
+                Section(header: Text("Order Information").bold()) {
+                    
+                    HStack {
+                        Image(systemName: "location")
+                            .font(Font.system(size: 25.0))
+                            .foregroundColor(.gray)
+                        
+                        Text("Add shipping address")
+                        
+                        Spacer()
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Image(systemName: "plus.circle.fill")
+                                .font(Font.system(size: 30.0))
+                            
+                        }
+                    }
+                    
+                }
+                
+            }
+
+            .navigationBarTitle("Payment & Orders", displayMode: .inline)
+        }
     }
 }
 
